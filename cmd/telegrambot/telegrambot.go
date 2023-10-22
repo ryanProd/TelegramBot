fa9cc6d1e783884dde37e1ab5424c9db88f7cda0
ryanProd <ryanli.softwarecandidate@gmail.com>
2023-10-22 15:24:15 -0400

    Working on Google Cloud, able to receive and log updates

--- cmd/telegrambot/telegrambot.go
+++ cmd/telegrambot/telegrambot.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/ryanProd/TelegramBot/pkg/handler"
-)
-
 func main() {
-	fmt.Println(handler.Handler())
+	return
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,5 +1,52 @@
 package handler
 
-func Handler() int {
-	return 10
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Update struct {
+	UpdateId int     `json:"update_id"`
+	Message  Message `json:"message"`
+}
+
+type Message struct {
+	MessageId int    `json:"message_id"`
+	Text      string `json:"text"`
+	From      User   `json:"from"`
+	Chat      Chat   `json:"chat"`
+}
+
+type Chat struct {
+	ChatId int    `json:"id"`
+	Type   string `json:"type"`
+}
+
+type User struct {
+	UserId   int    `json:"id"`
+	Username string `json:"username"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	var update, err = decodeRequest(r)
+	if err != nil {
+		log.Printf("could not decode update %s", err.Error())
+	}
+	log.Printf("Update Received")
+	log.Printf("Update ID is %d", update.UpdateId)
+	log.Printf("Message ID is %d", update.Message.MessageId)
+	log.Printf("Message Text is %s", update.Message.Text)
+	log.Printf("User ID is %d", update.Message.From.UserId)
+	log.Printf("Chat ID is %d", update.Message.Chat.ChatId)
+
+}
+
+func decodeRequest(r *http.Request) (*Update, error) {
+	var update Update
+	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+		log.Printf("could not decode update %s", err.Error())
+		return nil, err
+	}
+	return &update, nil
 }
