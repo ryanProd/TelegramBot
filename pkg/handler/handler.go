a64f7d90f0f0c5886d06bbf011cdc78f6e342b9f
ryanProd <ryanli.softwarecandidate@gmail.com>
2023-10-22 18:36:31 -0400

    sendMessage working

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,9 +2,19 @@ package handler
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"strconv"
 )
+
+const telegramApiBaseUrl string = "https://api.telegram.org/bot"
+const telegramApiSendMessage string = "/sendMessage"
+const telegramTokenEnv string = "BOT_TOKEN"
+
+var telegramEndpoint string = telegramApiBaseUrl + os.Getenv(telegramTokenEnv) + telegramApiSendMessage
 
 type Update struct {
 	UpdateId int     `json:"update_id"`
@@ -33,6 +43,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("could not decode update %s", err.Error())
 	}
+
 	log.Printf("Update Received")
 	log.Printf("Update ID is %d", update.UpdateId)
 	log.Printf("Message ID is %d", update.Message.MessageId)
@@ -40,6 +51,21 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("User ID is %d", update.Message.From.UserId)
 	log.Printf("Chat ID is %d", update.Message.Chat.ChatId)
 
+	if update.Message.Text == "/start" {
+		response, err := sendMessage(update.Message.Chat.ChatId, "Hello Welcome to Ryan's Chat Bot!")
+		if err != nil {
+			log.Printf(err.Error())
+		} else {
+			log.Printf(response)
+		}
+	} else {
+		response, err := sendMessage(update.Message.Chat.ChatId, update.Message.Text)
+		if err != nil {
+			log.Printf(err.Error())
+		} else {
+			log.Printf(response)
+		}
+	}
 }
 
 func decodeRequest(r *http.Request) (*Update, error) {
@@ -49,4 +75,27 @@ func decodeRequest(r *http.Request) (*Update, error) {
 		return nil, err
 	}
 	return &update, nil
+}
+
+func sendMessage(chatId int, text string) (string, error) {
+	log.Printf("Sending %s to ChatID: %d", text, chatId)
+
+	response, err := http.PostForm(
+		telegramEndpoint,
+		url.Values{
+			"chat_id": {strconv.Itoa(chatId)},
+			"text":    {text},
+		})
+	if err != nil {
+		log.Printf("Message to ChatID: %d failed to send. %s", chatId, err.Error())
+	}
+	defer response.Body.Close()
+
+	body, responseErr := ioutil.ReadAll(response.Body)
+	if responseErr != nil {
+		log.Printf(responseErr.Error())
+	}
+
+	return string(body), responseErr
+
 }
