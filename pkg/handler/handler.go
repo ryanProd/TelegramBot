a4fc235e550b8b9fe31c5acd131a6c1745a498a4
ryanProd <ryanli.softwarecandidate@gmail.com>
2023-10-23 16:47:18 -0400

    Added state logic

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -10,12 +10,29 @@ import (
 	"strconv"
 )
 
+// Constants and Struct for storing State
+const stateZero string = "Please review our product."
+const stateOne string = "Would you like to leave additional feedback?"
+const close string = "Thank you for being a valued customer, if you have additional questions please visit our website https://nike.com"
+
+type UserState struct {
+	UserId         int
+	currState      string
+	stateZeroReply string
+	stateOneReply  string
+}
+
+// map that stores state with the UserId as keys
+var userStates = make(map[int]*UserState, 20)
+
+// Endpoint strings
 const telegramApiBaseUrl string = "https://api.telegram.org/bot"
 const telegramApiSendMessage string = "/sendMessage"
 const telegramTokenEnv string = "BOT_TOKEN"
 
 var telegramEndpoint string = telegramApiBaseUrl + os.Getenv(telegramTokenEnv) + telegramApiSendMessage
 
+// Structs to decode Updates from telegram
 type Update struct {
 	UpdateId int     `json:"update_id"`
 	Message  Message `json:"message"`
@@ -38,6 +55,7 @@ type User struct {
 	Username string `json:"username"`
 }
 
+// Entry point to Cloud Function
 func Handler(w http.ResponseWriter, r *http.Request) {
 	var update, err = decodeRequest(r)
 	if err != nil {
@@ -51,30 +69,40 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("User ID is %d", update.Message.From.UserId)
 	log.Printf("Chat ID is %d", update.Message.Chat.ChatId)
 
-	handleUserInput(update)
-
+	_, _ = handleUserInput(update)
 }
 
-func handleUserInput(update *Update) {
-	var response string
-	var err error
-	if update.Message.Text == "/start" {
-		response, err = sendMessage(update.Message.Chat.ChatId, "Hello Welcome to Ryan's Chat Bot!")
-		if err != nil {
-			log.Printf(err.Error())
-		} else {
-			log.Printf(response)
-		}
+// in real implementation this function would call database to get User State and store responses
+// we could also send requests to our ai endpoint to integrate ai functionality here
+// store User state and send appropriate message back to them
+func handleUserInput(update *Update) (string, error) {
+	userId := update.Message.From.UserId
+	val, keyExists := userStates[userId]
+	if !keyExists || update.Message.Text == "/start" {
+		var temp UserState
+		temp.UserId = userId
+		temp.currState = stateZero
+		userStates[userId] = &temp
 	} else {
-		response, err = sendMessage(update.Message.Chat.ChatId, update.Message.Text)
-		if err != nil {
-			log.Printf(err.Error())
-		} else {
-			log.Printf(response)
+		log.Printf(val.currState)
+		if val.currState == stateZero {
+			userStates[userId].currState = stateOne
+			userStates[userId].stateZeroReply = update.Message.Text
+		} else if val.currState == stateOne {
+			userStates[userId].currState = close
+			userStates[userId].stateOneReply = update.Message.Text
 		}
 	}
+	responseBody, err := sendMessage(update.Message.Chat.ChatId, userStates[userId].currState)
+	if err != nil {
+		log.Printf(err.Error())
+	} else {
+		log.Printf("Message sent succesfully, Response Body: %s", responseBody)
+	}
+	return responseBody, err
 }
 
+// Destructure Update JSON into our structs
 func decodeRequest(r *http.Request) (*Update, error) {
 	var update Update
 	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
@@ -84,6 +112,7 @@ func decodeRequest(r *http.Request) (*Update, error) {
 	return &update, nil
 }
 
+// Send Message back to User
 func sendMessage(chatId int, text string) (string, error) {
 	log.Printf("Sending %s to ChatID: %d", text, chatId)
 
